1910561b3adaa0e1206679d918d60333f5496b41
Patrick Coleman <pcoleman@pixafy.com>
2017-01-10 10:39:28 -0500

    added baseUrls and staging option to the config object.

--- src/config.js
+++ src/config.js
@@ -1,3 +1,9 @@
+const baseUrls = {
+	development: 'http://localhost:3030/',
+	staging: 'http://104.131.34.211/',
+	production: 'http://www.product-video-feeds.com/'
+};
+
 const config = {
     development: {
         db: 'mongodb://localhost/sample_api', // -> host/database_name[/port][/options]
@@ -5,19 +11,31 @@ const config = {
         redis: {},
         oauth: {
 			sharedSecret: '7969310b79a5e99b21349e2097a5ff40',
-			redirectUri: 'http://localhost:3030/auth/grant',
+			redirectUri: baseUrls['development']+'auth/grant',
+			apiKey: 'db7c687e9e9afbed4cffb118951c7f29',
+			scopes: ['read_products', 'write_products', 'read_script_tags', 'write_script_tags']
+		}
+    },
+
+    staging: {
+        db: 'mongodb://localhost/sample_api', // -> host/database_name[/port][/options]
+        session_secret: '1wEEnAh11',
+        redis: {},
+        oauth: {
+			sharedSecret: '7969310b79a5e99b21349e2097a5ff40',
+			redirectUri: baseUrls['staging']+'auth/grant',
 			apiKey: 'db7c687e9e9afbed4cffb118951c7f29',
 			scopes: ['read_products', 'write_products', 'read_script_tags', 'write_script_tags']
 		}
     },
 
     production: {
-        db: 'mongodb://localhost/sample_api',
+        db: 'mongodb://localhost/sample_api', // -> host/database_name[/port][/options]
         session_secret: '1wEEnAh11',
         redis: {},
         oauth: {
 			sharedSecret: '7969310b79a5e99b21349e2097a5ff40',
-			redirectUri: 'http://localhost:3030/auth/grant',
+			redirectUri: baseUrls['production']+'auth/grant',
 			apiKey: 'db7c687e9e9afbed4cffb118951c7f29',
 			scopes: ['read_products', 'write_products', 'read_script_tags', 'write_script_tags']
 		}
